a3466f503c3b2d268cf929115b7afac2ab123b21
Benjamin Aas <battlebeast.overwatch@gmail.com>
2024-03-13 17:30:44 +0100

    Trying to configure the compiler

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
+	int x = 0;
+	std::cout << "Enter a number: ";
+	std::cin >> x;
+	std::cout << "You entered " << x << std::endl;
 	std::cout << "Hello, world!";
 	return 0;
 }
